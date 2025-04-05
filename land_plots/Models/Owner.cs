7057f24e4334acd9d95a9240845afa3878f8d9d9
Roman_ko2024 <manko.roman1123@vu.cdu.edu.ua>
2025-04-05 13:29:33 +0300

    Owner.cs rework

--- land_plots/Models/Owner.cs
+++ land_plots/Models/Owner.cs
@@ -1,17 +1,23 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
-namespace land_plots.Models
+namespace LandManagementApp.Models
 {
-    public class Owner
+    [DataContract] //для серіалізації
+    //щоб зберігати у форматі JSON/XML
+    public class Owner : INotifyPropertyChanged, IDataErrorInfo
     {
         private string _firstName;
         private string _lastName;
         private DateTime _birthDate;
+
+        //подія для оновлення GUI
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //ПОРОЖНІЙ КОНСТРУКТОР ПОТРІБЕН ДЛЯ СЕРІАЛІЗАЦІЇЇ
+        public Owner() { }
 
         public Owner(string firstName, string lastName, DateTime birthDate)
         {
@@ -20,23 +26,87 @@ namespace land_plots.Models
             BirthDate = birthDate;
         }
 
+        [DataMember]
         [Required(ErrorMessage = "Ім'я обов'язкове")]
         public string FirstName
         {
             get => _firstName;
-            set => _firstName = !string.IsNullOrWhiteSpace(value)
-                ? value : throw new ArgumentException("Ім'я не може бути порожнім.");
+            set
+            {
+                if (_firstName != value)
+                {
+                    _firstName = !string.IsNullOrWhiteSpace(value)
+                        ? value
+                        : throw new ArgumentException("Ім'я не може бути порожнім.");
+                    OnPropertyChanged(nameof(FirstName));
+                }
+            }
         }
 
+        [DataMember]
         [Required(ErrorMessage = "Прізвище обов'язкове")]
         public string LastName
         {
             get => _lastName;
-            set => _lastName = !string.IsNullOrWhiteSpace(value)
-                ? value : throw new ArgumentException("Прізвище не може бути порожнім.");
+            set
+            {
+                if (_lastName != value)
+                {
+                    _lastName = !string.IsNullOrWhiteSpace(value)
+                        ? value
+                        : throw new ArgumentException("Прізвище не може бути порожнім.");
+                    OnPropertyChanged(nameof(LastName));
+                }
+            }
         }
 
-        [Range(typeof(DateTime), "1900-01-01", "2100-01-01", ErrorMessage = "Невірна дата")]
-        public DateTime BirthDate { get; set; }
+        [DataMember]
+        [CustomValidation(typeof(Owner), nameof(ValidateBirthDate))]
+        public DateTime BirthDate
+        {
+            get => _birthDate;
+            set
+            {
+                if (_birthDate != value)
+                {
+                    _birthDate = value;
+                    OnPropertyChanged(nameof(BirthDate));
+                }
+            }
+        }
+
+        public static ValidationResult ValidateBirthDate(DateTime date)
+        {
+            return (date.Year >= 1900 && date.Year <= 2100)
+                ? ValidationResult.Success
+                : new ValidationResult("Допустимі роки: 1900-2100");
+        }
+
+        //INotifyPropertyChanged оновлює GUI при зміні властивостей
+        protected virtual void OnPropertyChanged(string propertyName)//метод інвокить подію
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        //інтерфейс IDataErrorInfo
+        //щоб не давало загальну помилку
+        public string Error => null;
+        public string this[string columnName]
+        {
+            get
+            {
+                switch (columnName)
+                {
+                    case nameof(FirstName):
+                        return string.IsNullOrEmpty(FirstName) ? "Ім'я обов'язкове" : null;
+                    case nameof(LastName):
+                        return string.IsNullOrEmpty(LastName) ? "Прізвище обов'язкове" : null;
+                    case nameof(BirthDate):
+                        return ValidateBirthDate(BirthDate).ErrorMessage;
+                    default:
+                        return null;
+                }
+            }
+        }
     }
 }
