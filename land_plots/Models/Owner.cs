436606704a3ff12216a6ee20705a2abc752f40a0
Roman_ko2024 <manko.roman1123@vu.cdu.edu.ua>
2025-04-05 12:39:17 +0300

    Owner

--- land_plots/Models/Owner.cs
+++ land_plots/Models/Owner.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,36 @@ using System.Threading.Tasks;
 
 namespace land_plots.Models
 {
-    internal class Owner
+    public class Owner
     {
+        private string _firstName;
+        private string _lastName;
+        private DateTime _birthDate;
+
+        public Owner(string firstName, string lastName, DateTime birthDate)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+        }
+
+        [Required(ErrorMessage = "Ім'я обов'язкове")]
+        public string FirstName
+        {
+            get => _firstName;
+            set => _firstName = !string.IsNullOrWhiteSpace(value)
+                ? value : throw new ArgumentException("Ім'я не може бути порожнім.");
+        }
+
+        [Required(ErrorMessage = "Прізвище обов'язкове")]
+        public string LastName
+        {
+            get => _lastName;
+            set => _lastName = !string.IsNullOrWhiteSpace(value)
+                ? value : throw new ArgumentException("Прізвище не може бути порожнім.");
+        }
+
+        [Range(typeof(DateTime), "1900-01-01", "2100-01-01", ErrorMessage = "Невірна дата")]
+        public DateTime BirthDate { get; set; }
     }
 }
