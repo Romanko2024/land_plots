1092b966b11f9c8c78773e29fe47fbc8459d532c
Roman_ko2024 <manko.roman1123@vu.cdu.edu.ua>
2025-04-05 12:30:56 +0300

    PurposeType

--- land_plots/Models/PurposeType.cs
+++ land_plots/Models/PurposeType.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace land_plots.Models
 {
-    internal class PurposeType
+    public enum PurposeType
     {
+        [Description("Під забудову")]
+        Construction,
+        [Description("Сільськогосподарського призначення")]
+        Agricultural,
+        [Description("Зарезервована")]
+        Reserved
     }
 }
