39d80a2070a16d72157b05711a7694f2e7a5f8b6
Roman_ko2024 <manko.roman1123@vu.cdu.edu.ua>
2025-04-27 22:43:29 +0300

    NullToFalseConverter

---
+++ land_plots/Utils/NullToFalseConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace LandManagementApp.Utils
+{
+    public class NullToFalseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => value != null;  // повертає true якщо об'єкт не null
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotSupportedException();
+    }
+}
