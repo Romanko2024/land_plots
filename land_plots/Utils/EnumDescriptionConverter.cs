5373048637b1bd6b092378840c2e3c9e5360efb4
Roman_ko2024 <manko.roman1123@vu.cdu.edu.ua>
2025-04-27 21:35:22 +0300

    fixed error XDG0010

---
+++ land_plots/Utils/EnumDescriptionConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace LandManagementApp.Utils
+{
+    //public class EnumDescriptionConverter : IValueConverter
+    //{
+    //    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    //    {
+            
+    //    }
+
+    //    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    //    {
+            
+    //    }
+    //}
+}
